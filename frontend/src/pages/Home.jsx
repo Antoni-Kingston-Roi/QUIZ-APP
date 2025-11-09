2ebbec7da8cfc0411e14285d6e38f78d53b9054f
Antoni-Kingston-Roi <antonikingstonj@gmail.com>
2025-11-09 23:25:38 +0530

    first commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from "../components/Navbar.jsx";
+import  Sidebar  from '../components/Sidebar.jsx';
+
+const Home = () => {
+  return (
+    <div>
+
+        <Navbar/>
+        <Sidebar/>
+    </div>
+  )
+}
+
+export default Home
